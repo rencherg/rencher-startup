07f80b00e8a27c8cfc37196acc208bb327332cc1
rencherga <rencher.grant@gmail.com>
2024-03-11 18:47:45 -0600

    Fixed bug

--- html/js/displaypostlinks.js
+++ html/js/displaypostlinks.js
@@ -3,14 +3,50 @@ posts=JSON.parse(postData)
 
 let divParent = document.querySelector('.main-link');
 
-posts["posts"].forEach(post => {
+function loadData() {
+    return new Promise((resolve, reject) => {
 
-    let aTag = document.createElement("a");
-    aTag.textContent = post["message"];
-    let pTag = document.createElement("p");
-    pTag.textContent = 'By '+post["user"];
+        if (!localStorage.getItem("dataLoaded")){
 
-    aTag.href = 'post.html?id='+post["id"]
-    divParent.appendChild(aTag);
-    divParent.appendChild(pTag);
-});
+            fetch('/data', {
+                method: 'GET',
+            })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+        
+                return response.json()
+            }).then(responseData => {
+                localStorage.setItem("samplePostData", JSON.stringify(responseData.samplePostData));
+                localStorage.setItem("sampleUsers", JSON.stringify(responseData.sampleUsers));
+                localStorage.setItem("dataLoaded", true);
+                localStorage.setItem("sampleWebsocketData", JSON.stringify(responseData.sampleWebsocketData));
+        
+            })
+            .catch(error => {
+                // Handle error here
+                console.error('There was a problem with your fetch operation:', error);
+            });
+        }
+
+        resolve()
+
+    });
+}
+
+loadData().then(()=>{
+    posts["posts"].forEach(post => {
+
+        let aTag = document.createElement("a");
+        aTag.textContent = post["message"];
+        let pTag = document.createElement("p");
+        pTag.textContent = 'By '+post["user"];
+    
+        aTag.href = 'post.html?id='+post["id"]
+        divParent.appendChild(aTag);
+        divParent.appendChild(pTag);
+    });
+}).catch(error =>{
+    console.error(error)
+})
