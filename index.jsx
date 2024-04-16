315044363da12cd4ad4bcb796968a19110c02244
rencherga <rencher.grant@gmail.com>
2024-04-16 17:39:19 -0600

    Some improvements, we are making progress

---
+++ index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './src/app';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
+// Already included file name '/Users/rencherga/Desktop/260/rencher-startup/src/app.jsx' differs from file name '/Users/rencherga/Desktop/260/rencher-startup/src/App.jsx' only in casing.
+//   The file is in the program because:
+//     Imported via './src/app' from file '/Users/rencherga/Desktop/260/rencher-startup/index.jsx'
+//     Root file specified for compilation
+
+// What needs to be done?
+// More code in app.jsx
+// OG code in app.jsx
+// App.jsx router
+// App functioning
+
+
